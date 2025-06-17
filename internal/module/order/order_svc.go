dce8d22e82175e59408a60e9f7e6f38c41d1d775
MaiWittawat <wittawat.pin@ku.th>
2025-06-17 13:06:44 +0700

    refactor: refactor log order_svc.go

--- internal/module/order/order_svc.go
+++ internal/module/order/order_svc.go
@@ -21,6 +21,7 @@ var (
 
 	ErrOrderNotFound = errors.New("order not found")
 	ErrChangeProduct = errors.New("can not change product")
+	ErrPermission    = errors.New("no permission can't delete another order")
 )
 
 type orderService struct {
@@ -42,35 +43,38 @@ func NewOrderService(ordeRepo repository.OrderRepository, productSvc module.Prod
 func (s *orderService) Save(ctx context.Context, oReq *model.OrderReq, userID string) error {
 	productResp, err := s.productSvc.GetByID(ctx, oReq.ProductID)
 	if err != nil {
-		return err
+		return ErrCreateOrder
 	}
 
 	order := oReq.ToOrder(userID, productResp)
+
 	var baseLogFields = log.Fields{
 		"order_id": order.ID,
 		"layer":    "order_service",
-		"step":     "order_save",
+		"method":   "order_save",
 	}
 
 	if err := s.orderRepo.AddOrder(ctx, order); err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to save order")
+		log.WithError(err).WithFields(baseLogFields).Error("save order")
 		return ErrCreateOrder
 	}
 	log.Info("[Service]: Order created success:", order)
 
 	bodyByte, err := json.Marshal(model.Stock{ProductID: order.ProductID, Quantity: order.Quantity})
 	if err != nil {
-		return err
+		log.WithError(err).WithFields(baseLogFields).Error("json marshal")
+		return ErrCreateOrder
 	}
 
 	mqConf := &model.MQConfig{
-		ExchangeName: messagebroker.StockExchangeName, 
-		ExchangeType: messagebroker.StockExchangeType, 
-		QueueName: messagebroker.StockQueueName, 
-		RoutingKey: "stock.decrease",
+		ExchangeName: messagebroker.StockExchangeName,
+		ExchangeType: messagebroker.StockExchangeType,
+		QueueName:    messagebroker.StockQueueName,
+		RoutingKey:   "stock.decrease",
 	}
 	if err := s.producerSvc.Publishing(ctx, mqConf, bodyByte); err != nil {
-		return err
+		log.WithError(err).WithFields(baseLogFields).Error("publishing")
+		return ErrCreateOrder
 	}
 
 	return nil
@@ -85,22 +89,23 @@ func (s *orderService) Update(ctx context.Context, orderReq *model.Order, id str
 
 	product, err := s.productSvc.GetByID(ctx, orderReq.ProductID)
 	if err != nil {
-		return err
+		return ErrUpdateOrder
 	}
 
 	var currentOrder model.Order
 	if err := s.orderRepo.GetOrderByID(ctx, id, &currentOrder); err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to get order by id")
+		log.WithError(err).WithFields(baseLogFields).Error("get order by id")
 		return ErrOrderNotFound
 	}
 
 	if product.ID != currentOrder.ID {
-		return ErrChangeProduct
+		log.WithError(ErrChangeProduct).WithFields(baseLogFields)
+		return ErrUpdateOrder
 	}
 
 	currentOrder.UpdatedAt = time.Now()
 	if err := s.orderRepo.UpdateOrder(ctx, &currentOrder, id); err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to update order")
+		log.WithError(err).WithFields(baseLogFields).Error("update order")
 		return ErrUpdateOrder
 	}
 
@@ -112,38 +117,41 @@ func (s *orderService) Delete(ctx context.Context, id string, userID string) err
 	var baseLogFields = log.Fields{
 		"order_id": id,
 		"layer":    "order_service",
-		"step":     "order_delete",
+		"method":   "order_delete",
 	}
-	log.Info("userID in delete: ", userID)
+
 	var order model.Order
 	if err := s.orderRepo.GetOrderByID(ctx, id, &order); err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to get order by id")
+		log.WithError(err).WithFields(baseLogFields).Error("get order by id")
 		return ErrOrderNotFound
 	}
 
 	if order.UserID != userID {
-		log.WithError(errors.New("no permission")).WithFields(baseLogFields).Error("can't delete another order")
+		log.WithError(ErrPermission).WithFields(baseLogFields)
 		return ErrDeleteOrder
 	}
 
 	if err := s.orderRepo.DeleteOrder(ctx, id); err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to delete order")
+		log.WithError(err).WithFields(baseLogFields).Error("delete order")
 		return ErrDeleteOrder
 	}
 	log.Info("[Service]: order deleted success:", order)
 
 	bodyByte, err := json.Marshal(model.Stock{ProductID: order.ProductID, Quantity: order.Quantity})
 	if err != nil {
+		log.WithError(err).WithFields(baseLogFields).Error("json marshal")
 		return err
 	}
 
 	mqConf := &model.MQConfig{
-		ExchangeName: messagebroker.StockExchangeName, 
-		ExchangeType: messagebroker.StockExchangeType, 
-		QueueName: messagebroker.StockQueueName, 
-		RoutingKey: "stock.increase",
+		ExchangeName: messagebroker.StockExchangeName,
+		ExchangeType: messagebroker.StockExchangeType,
+		QueueName:    messagebroker.StockQueueName,
+		RoutingKey:   "stock.increase",
 	}
+
 	if err := s.producerSvc.Publishing(ctx, mqConf, bodyByte); err != nil {
+		log.WithError(err).WithFields(baseLogFields).Error("publishing")
 		return err
 	}
 
@@ -153,13 +161,13 @@ func (s *orderService) Delete(ctx context.Context, id string, userID string) err
 // ------------------------ Method Basic Query ------------------------
 func (s *orderService) GetAll(ctx context.Context) ([]model.OrderResp, error) {
 	var baseLogFields = log.Fields{
-		"layer": "order_service",
-		"step":  "order_getAll",
+		"layer":  "order_service",
+		"method": "order_getAll",
 	}
 
 	orders, err := s.orderRepo.GetAllOrder(ctx)
 	if err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to get all order")
+		log.WithError(err).WithFields(baseLogFields).Error("get all order")
 		return nil, ErrOrderNotFound
 	}
 
@@ -178,12 +186,12 @@ func (s *orderService) GetByID(ctx context.Context, id string) (*model.OrderResp
 	var baseLogFields = log.Fields{
 		"order_id": id,
 		"layer":    "order_service",
-		"step":     "order_delete",
+		"method":   "order_delete",
 	}
 
 	err := s.orderRepo.GetOrderByID(ctx, id, &order)
 	if err != nil {
-		log.WithError(err).WithFields(baseLogFields).Error("failed to get order by id")
+		log.WithError(err).WithFields(baseLogFields).Error("get order by id")
 		return nil, ErrOrderNotFound
 	}
 
